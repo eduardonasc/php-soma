7c78e428374211885099fac50714e59558bc40c0
Eduardo Nascimento <eduardo.nascimento@mazzafc.tech>
2021-07-21 21:35:12 -0300

    Adicionados arquivos inicias

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        * {
+            margin: 0;
+            padding: 0;
+        }
+
+        body {
+            height: 100vh;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-image: url(https://images.unsplash.com/photo-1608792992053-f397e328a56d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1934&q=80);
+        }
+
+        form {
+            height: 150px;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-around;
+            align-items: center;
+            background-color: #bbb;
+            border-radius: 10px;
+        }
+    </style>
+</head>
+<body>
+    <form action="soma.php" method="post">
+        <h1>Calculadora</h1>
+        <input type="number" name="num1">
+        <input type="number" name="num2">
+        <button type="submit">Somar</button>
+    </form>
+</body>
+</html>
---
+++ soma.php
@@ -0,0 +1,10 @@
+<?php
+
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+
+    $soma = $num1 + $num2;
+
+    echo 'SOMA = ' . $soma;
+
+?>
